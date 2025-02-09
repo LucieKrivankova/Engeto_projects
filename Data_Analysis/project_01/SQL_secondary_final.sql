e59419129b104b1acd42efd87ec05622545fd190
Lucie Křivánková <luciekrivankova.10@gmail.com>
2025-02-09 18:48:51 +0100

    Create SQL_secondary_final.sql

---
+++ Data_Analysis/project_01/SQL_secondary_final.sql
@@ -0,0 +1,16 @@
+-- create table t_lucie_krivankova_project_SQL_secondary_final as (
+select 
+	e.year,
+	e.country,
+	e.gdp,
+	e.taxes,
+	e.gini,
+	e.population
+from countries c
+left join economies e
+	on c.country = e.country
+where 1=1
+	and c.continent = 'Europe'
+	and e.year between 2006 and 2018
+--)
+;
